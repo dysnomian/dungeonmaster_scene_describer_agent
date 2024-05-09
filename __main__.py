efd21e9a7c372a5fa56ac9a8c8ca776da74fbdb4
Liss McCabe <liss@eristiccode.com>
2024-05-09 12:07:25 -0700

    initial commit

---
+++ __main__.py
@@ -0,0 +1,135 @@
+import os
+import asyncio
+import json
+
+from typing import Any, Union
+
+import psycopg2
+import nats
+from openai import OpenAI
+
+NATS_SERVER = os.getenv('NATS_SERVER', 'nats://localhost:4222')
+INCOMING_NATS_SUBJECT = os.getenv('NATS_SUBJECT', 'scenes')
+OUTGOING_NATS_SUBJECT = os.getenv('NATS_SUBJECT', 'descriptions')
+
+DB_NAME = os.getenv('DUNGEONMASTER_DB_NAME', 'dungeonmaster_dev')
+DB_HOST = os.getenv('DUNGEONMASTER_DB_HOST', 'localhost')
+DB_USER = os.getenv('DUNGEONMASTER_DB_USER', 'dungeonmaster')
+DB_PASSWORD = os.getenv('DUNGEONMASTER_DB_PASSWORD', 'dungeonmaster')
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+LLM_CONFIG = {
+    "model": "gpt-3.5-turbo"
+}   
+
+def db_client():
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST
+    )
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+### Database
+
+def fetch_location(
+        location_id: int,
+        session_id: Union[int, None] = None
+        ) -> None | str:
+    "Given a location ID, fetches the location from the database and returns it as JSON."
+    location = None
+    exits = []
+
+    with db_client() as conn:
+        with conn.cursor() as cur:
+
+            # Get the base location info
+            cur.execute(
+                "SELECT * FROM locations WHERE id = %s",
+                (location_id,)
+            )
+            result = cur.fetchone() or {}
+            location = {
+                "id": result[0],
+                "starting_location_id": result[1],
+                "parent_id": result[2],
+                "name": result[3],
+                "category": result[4],
+                "interior_description": result[5],
+                "exterior_description": result[6],
+                "notes": result[7]
+            }
+            print("LOCATION:", location)
+
+            # Get the exits where the start_location_id or end_location_id is the location_id
+            if location is not None:
+                cur.execute(
+                    """
+                    SELECT id, start_location_id, end_location_id, name, description, category, is_one_way, is_fast_travel_path, describe_end_location_exterior
+                    FROM connections
+                    WHERE start_location_id = %s OR end_location_id = %s
+                    """,
+                    (location_id, location_id)
+                )
+                result = cur.fetchall()
+
+                if result is not None:
+                    for row in result:
+                        exits.append({
+                            "id": row[0],
+                            "start_location_id": row[1],
+                            "end_location_id": row[2],
+                            "name": row[3],
+                            "description": row[4],
+                            "category": row[5],
+                            "is_one_way": row[6],
+                            "is_fast_travel_path": row[7],
+                            "describe_end_location_exterior": row[8],
+                        })
+                    location["exits"] = exits
+                print("EXITS:", location["exits"])
+
+            # Get the game session info if id is provided
+            cur.execute(
+                "SELECT * FROM game_sessions WHERE id = %s",
+                (session_id,)
+            )
+            result = cur.fetchone()
+            print("TIME OF DAY:", result)
+            if result is not None:
+                location["current_time"] = result[0]
+    
+    if location is None:
+        return None
+    else:
+        return json.dumps(location)
+
+### Agent
+
+system_prompt = """
+You are an agent that takes JSON scene descriptions and fleshes them out into full vivid descriptions for a story. Give your description in the second person ('You see a room with...', 'The tree towers before you...') and write them using the interior_description field as the basis. The notes are to guide your description, but should be concealed from the player and should not be mentioned explicitly.
+
+If the time of day is included, take that into account in your description. Consider differences in noises, activitites, lighting.
+
+Mention all non-hidden exits, but do not make up any. If an exit specifies to describe the exit end location's exterior, do so.
+"""
+
+user_prompt = fetch_location(23, 1)
+
+response = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": str(user_prompt)},
+            ],
+            model=LLM_CONFIG.get("model", "gpt-3.5-turbo"),
+            temperature=1,
+            max_tokens=1024,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+
+print("LOCATION JSON:\n", user_prompt)
+print("RESPONSE:\n", response.choices[0].message.content)
